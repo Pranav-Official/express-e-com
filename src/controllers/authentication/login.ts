ce9117c1d7d79a618e2b20a4a8486fe3d93a9b6b
Pranav-Official <pranavhtjp07@gmail.com>
2024-02-12 09:44:41 +0530

    feat:added customer

--- src/controllers/authentication/login.ts
+++ src/controllers/authentication/login.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import EcSuppliers from "../../models/ec_suppliers";
+import Customers from "../../models/customers";
 
 export const login = async (
   req: Request,
@@ -31,6 +32,36 @@ export const login = async (
         return res.status(200).json({
           token,
           registration_id: supplier.registration_id,
+        });
+      } else {
+        return res.status(400).send("Invalid credentials");
+      }
+    } catch (err: any) {
+      console.log(err);
+      return res.status(400).send("Error");
+    }
+  }
+  if (req.body.user_type === "customer") {
+    try {
+      const { e_mail, password } = req.body;
+      const customers = await Customers.findOne({
+        where: { e_mail },
+        raw: true,
+      });
+      if (customers && bcrypt.compareSync(password, customers.password)) {
+        const token = jwt.sign(
+          {
+            registration_id: customers.registration_id,
+            user_type: req.body.user_type,
+          },
+          "my-secret-key",
+          {
+            expiresIn: "24h",
+          }
+        );
+        return res.status(200).json({
+          token,
+          registration_id: customers.registration_id,
         });
       } else {
         return res.status(400).send("Invalid credentials");
